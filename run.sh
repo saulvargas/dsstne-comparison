f7c7d9c7802ce7107e1e462269e8ce090ed6e796
Saúl <saul.vargas@glasgow.ac.uk>
2016-06-04 12:58:28 +0100

    added experiment with DSSTNE example network

--- run.sh
+++ run.sh
@@ -30,19 +30,22 @@ cut -f2 train.data test.data | sort -u > items.txt
 # compile java project
 mvn clean package
 
-# run and evaluate ranksys recommendations
+# run ranksys recommendations
 java -Xmx6G -jar target/dsstne-comparison-0.1.jar compute
-java -Xmx6G -jar target/dsstne-comparison-0.1.jar evaluate pop rnd mf ub
 
 # convert training data to DSSTNE format
 java -Xmx6G -jar target/dsstne-comparison-0.1.jar to-dsstne-format
 
 # run DSSTNE with training data
-# TO DO
+generateNetCDF -d gl_input -i train.dsstne -o gl_input.nc -f features_input -s samples_input -c
+generateNetCDF -d gl_output -i train.dsstne -o gl_output.nc -f features_output -s samples_input -c
+wget https://s3-us-west-2.amazonaws.com/amazon-dsstne-samples/configs/config.json
+train -c config.json -i gl_input.nc -o gl_output.nc -n gl.nc -b 256 -e 10
+predict -b 1024 -d gl -i features_input -o features_output -k 100 -n gl.nc -f train.dsstne -s predictions.dsstne -r train.dsstne
 
 # convert generated recommendations to ranksys format
-#java -Xmx6G -jar target/dsstne-comparison-0.1.jar from-dsstne-format < predictions.dsstne > dsstne
+java -Xmx6G -jar target/dsstne-comparison-0.1.jar from-dsstne-format < predictions.dsstne > dsstne
 
-# evaluate DSSTNE recommendations
-#java -Xmx6G -jar target/dsstne-comparison-0.1.jar evaluate dsstne
+# evaluate recommendations
+java -Xmx6G -jar target/dsstne-comparison-0.1.jar evaluate pop rnd mf ub dsstne
 

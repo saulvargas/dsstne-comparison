6a270cf527b3c0623fbcc4e369b0f962337bc6db
Saul Vargas <Saul.Vargas@mendeley.com>
2016-08-26 09:47:48 +0000

    added benchmark #2 nn config file, last version of ranksys

--- run.sh
+++ run.sh
@@ -39,7 +39,7 @@ java -Xmx6G -jar target/dsstne-comparison-0.1.jar to-dsstne-format
 # run DSSTNE with training data
 generateNetCDF -d gl_input -i train.dsstne -o gl_input.nc -f features_input -s samples_input -c
 generateNetCDF -d gl_output -i train.dsstne -o gl_output.nc -f features_output -s samples_input -c
-wget https://s3-us-west-2.amazonaws.com/amazon-dsstne-samples/configs/config.json
+#wget https://s3-us-west-2.amazonaws.com/amazon-dsstne-samples/configs/config.json
 train -c config.json -i gl_input.nc -o gl_output.nc -n gl.nc -b 256 -e 10
 predict -b 1024 -d gl -i features_input -o features_output -k 100 -n gl.nc -f train.dsstne -s predictions.dsstne -r train.dsstne
 
